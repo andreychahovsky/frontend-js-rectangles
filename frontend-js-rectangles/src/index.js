2114fc03db02429f2da8fb48e62a9d6d3eadb214
andreychahovsky <chahovsky.andrey@outlook.com>
2016-12-27 22:33:51 +0200

    function filterVisible() is added

--- frontend-js-rectangles/src/index.js
+++ frontend-js-rectangles/src/index.js
@@ -1,9 +1,39 @@
-﻿function areIntersected(rectangle1, rectangle2) {
+﻿function isEmptyRectangle(width, height) {
+	if ((width === 0) && (height === 0)) {
+		return true;
+	}
+}
+
+function isNotRectangle(width, height) {
+	if ((width === 0) || (height === 0)) {
+		return true;
+	}
+}
+
+function filterVisible(rectangle1, arrayOfRectangles) {
+	var positiveArrayOfRectangles;
+
+	positiveArrayOfRectangles = arrayOfRectangles.filter(function (rectangle2) {
+		if (isNotRectangle(rectangle2.width, rectangle2.height)) {
+			return false;
+		}
+		if (areIntersected(rectangle1, rectangle2)) {
+			return true;
+		}
+		if (isEmptyRectangle(rectangle1.width, rectangle1.height)) {
+			return false;
+		}
+	});
+
+	return positiveArrayOfRectangles;
+}
+
+function areIntersected(rectangle1, rectangle2) {
 
 	var rectangle1Right, rectangle1Bottom, rectangle2Right, rectangle2Bottom;
 	var horisontalIntersection, verticalIntersection;
 
-	if (((rectangle1.width === 0) && (rectangle1.height === 0)) || ((rectangle2.width === 0) && (rectangle2.height === 0))) {
+	if (isEmptyRectangle(rectangle1.width, rectangle1.height) || isEmptyRectangle(rectangle2.width, rectangle2.height))  {
 		return false;
 	}
 
@@ -14,11 +44,6 @@
 
 	horisontalIntersection = (rectangle1.left >= rectangle2.left) && (rectangle1.left < rectangle2Right) || (rectangle1Right > rectangle2.left) && (rectangle1Right <= rectangle2Right) || (rectangle2.left >=rectangle1.left) && (rectangle2.left < rectangle1Right);
 	verticalIntersection = (rectangle1.top >= rectangle2.top) && (rectangle1.top < rectangle2Bottom) || (rectangle1Bottom > rectangle2.top) && (rectangle1Bottom <= rectangle2Bottom) || (rectangle2.top >= rectangle1.top) && (rectangle2.top < rectangle1Bottom);
-
-	
-
-	// horisontalIntersection = (rectangle1Right > rectangle2.left && rectangle1Right < rectangle2Right) || (rectangle1.left > rectangle2.left && rectangle1.left < rectangle2Right) || (rectangle1.left === rectangle2.left && (rectangle1Bottom + rectangle1.width) === (rectangle2Bottom + rectangle2.width)) || (rectangle1Bottom > rectangle2Bottom && rectangle1.height < rectangle2.height) || (rectangle2Bottom > rectangle1Bottom && rectangle2.height < rectangle1.height);
-	// verticalIntersection = (rectangle1Bottom > rectangle2.top && rectangle1Bottom < rectangle2Bottom) || (rectangle1.top > rectangle2.top && rectangle1.top < rectangle2Bottom) || (rectangle1.top === rectangle2.top && (rectangle1Right + rectangle1.height) === (rectangle2Right + rectangle2.height))||  (rectangle1.left > rectangle2.left && rectangle1Right < rectangle2Right) ||  (rectangle2.left > rectangle1.left && rectangle2Right < rectangle1Right);
 
 	if (horisontalIntersection && verticalIntersection) {
 		return true;
